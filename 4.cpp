9b5bcae34efcbbf51fd912d197bd6ad5d6242324
yashkherodiya <yashkherodiya@gmail.com>
2024-12-11 11:53:04 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -23,4 +23,37 @@ class Circle : public Shape {
 public:
     // Providing implementation for pure virtual function
     void draw() override {
-        cout << "Drawing a 
+        cout << "Drawing a Circle." << endl;
+    }
+};
+
+// Derived class: Square
+class Square : public Shape {
+public:
+    // Providing implementation for pure virtual function
+    void draw() override {
+        cout << "Drawing a Square." << endl;
+    }
+};
+
+int main() {
+    // Cannot create an instance of Shape, since it is abstract
+    // Shape s;  // Error: Cannot instantiate abstract class
+
+    // Create objects of derived classes
+    Shape* circle = new Circle();
+    Shape* square = new Square();
+
+    // Calling overridden methods in derived classes
+    circle->draw();  // Calls Circle's draw()
+    square->draw();  // Calls Square's draw()
+
+    // Calling a method defined in the abstract class
+    circle->move();  // Calls Shape's move()
+
+    // Cleanup
+    delete circle;
+    delete square;
+
+    return 0;
+}
